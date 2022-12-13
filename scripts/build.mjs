7c5ee420ce10e2e0466f8e31c7b1a309fa3aa993
黄嘉铭 <347975891@qq.com>
2022-12-13 11:07:45 +0800

    debug

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,6 +3,7 @@ import esbuild from 'esbuild'
 esbuild.build({
   entryPoints: ['src/index.ts'],
   outfile: 'outfile.cjs',
+  // bundle: true,
   format: 'cjs',
   platform: 'node',
   target: 'node14'
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,57 @@
-import prompts from 'prompts'
-import minimist from 'minimist'
-import semver from 'semver'
-import { readFileSync } from 'fs'
-import path from 'path'
-import shell from 'shelljs'
+#!/usr/bin/env node
 
-async function init(){
-  const cwd = process.cwd()
+import prompts from "prompts";
+import semver from "semver";
+import { readFileSync } from "fs";
+import path from "path";
+import shell from "shelljs";
 
-  const argv = minimist(process.argv.slice(2), {
-    boolean: true
-  })
+async function init() {
+  const pkg = JSON.parse(
+    readFileSync(path.resolve(__dirname, "package.json"), "utf8")
+  );
 
-  console.log('argv', argv);  
-
-  const pkg = JSON.parse(readFileSync(path.resolve(__dirname, 'package.json'), 'utf8'))
-
-  function versionList(version){
-    const major = semver.inc(version, 'major')
-    const minor = semver.inc(version, 'minor')
-    const patch = semver.inc(version, 'patch')
-    const prerelease = semver.inc(version, 'prerelease', 'beta')
+  function versionList(version) {
+    const major = semver.inc(version, "major");
+    const minor = semver.inc(version, "minor");
+    const patch = semver.inc(version, "patch");
+    const prerelease = semver.inc(version, "prerelease", "beta");
 
     return [
-      {title: major, value: major},
-      {title: minor, value: minor},
-      {title: patch, value: patch},
-      {title: prerelease, value: prerelease},
-    ]
+      { title: major, value: major },
+      { title: minor, value: minor },
+      { title: patch, value: patch },
+      { title: prerelease, value: prerelease },
+    ];
   }
 
   const res = await prompts([
     {
-      message: '请选择版本号',
-      name: 'version',
-      type: 'select',
-      choices: versionList(pkg.version)
+      message: "请选择版本号",
+      name: "version",
+      type: "select",
+      choices: versionList(pkg.version),
     },
     {
-      message: '是否要添加tag？',
-      name: 'tag',
-      type: 'confirm',
+      message: "是否要添加tag？",
+      name: "tag",
+      type: "confirm",
     },
     {
-      message: '代码要合并到哪条分支上？',
-      name: 'branch',
-      type: 'text',
+      message: "代码要合并到哪条分支上？",
+      name: "branch",
+      type: "text",
       validate: (value) => {
-        if(value.trim()){
-          return true
-        }else{
-          return '请输入分支名'
+        if (value.trim()) {
+          return true;
+        } else {
+          return "请输入分支名";
         }
-      }
+      },
     },
-  ])
+  ]);
 
-  const { version, branch, tag } = res
+  const { version, branch, tag } = res;
   console.log(`
     版本号：${version}
     是否要添加tag: ${tag}
@@ -65,45 +60,48 @@ async function init(){
 
   const isConfirm = await prompts([
     {
-      message: '请确认以上信息是否正确',
-      name: 'value',
-      type: 'confirm'
-    }
-  ])
+      message: "请确认以上信息是否正确",
+      name: "value",
+      type: "confirm",
+    },
+  ]);
 
-  if(!isConfirm.value) process.exit(1)
-  const branchStr = shell.exec('git branch').stdout
-  const currentBranch = branchStr.slice(2, branchStr.match('\n').index)
+  if (!isConfirm.value) process.exit(1);
+  const branchStr = shell.exec("git branch").stdout;
+  const currentBranch = branchStr.slice(2, branchStr.match("\n").index);
 
-  console.log('currentBranch', currentBranch);
-  console.log('branch', branch);
-  
-  shell.exec('git add .')
-  shell.exec(`git commit -m 'chore: ${version}'`)
+  console.log("currentBranch", currentBranch);
+  console.log("branch", branch);
+
+  shell.exec("git add .");
+  shell.exec(`git commit -m 'chore: ${version}'`);
 
   console.log(`git push => ${currentBranch}`);
-  shell.exec(`git push -u origin ${currentBranch}`)
-  
-  if(currentBranch !== branch){
-    const res = shell.exec(`git checkout ${branch}`)
-    if(res.code !== 0 && res.stderr === `error: 路径规格 '${branch}' 未匹配任何 git 已知文件\n`){
-      shell.exec(`git checkout -b ${branch}`)
+  shell.exec(`git push -u origin ${currentBranch}`);
+
+  if (currentBranch !== branch) {
+    const res = shell.exec(`git checkout ${branch}`);
+    if (
+      res.code !== 0 &&
+      res.stderr === `error: 路径规格 '${branch}' 未匹配任何 git 已知文件\n`
+    ) {
+      shell.exec(`git checkout -b ${branch}`);
     }
-    shell.exec(`git merge ${currentBranch}`)
+    shell.exec(`git merge ${currentBranch}`);
     console.log(`git push => ${branch}`);
-    shell.exec(`git push -u origin ${branch}`)
+    shell.exec(`git push -u origin ${branch}`);
   }
 
-  if(tag){
-    console.log(`git tag => ${tag}`)
-    shell.exec(`git push origin ${tag}`)
+  if (tag) {
+    console.log(`git tag => ${tag}`);
+    shell.exec(`git push origin ${tag}`);
   }
 
-  if(currentBranch !== branch){
-    shell.exec(`git checkout ${currentBranch}`)
+  if (currentBranch !== branch) {
+    shell.exec(`git checkout ${currentBranch}`);
   }
 }
 
-init().catch(err => {
-  console.log('catch err', err);
-})
+init().catch((err) => {
+  console.log("catch err", err);
+});
